835a97c7770661dd273d9f48918ba0a40f360967
aovsybo <matgr2n@gmail.com>
2023-11-23 19:26:02 +0300

    added /orders/{request_id}/ detail endpoint and added auth check functionality to /add/

--- src/orders/router.py
+++ src/orders/router.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.auth.base_config import current_user
+from src.auth.models import User
 from src.database import get_async_session
 
 from src.orders.models import request
@@ -14,15 +16,26 @@ router = APIRouter(
 
 
 @router.get("/")
-async def get_requests(session: AsyncSession = Depends(get_async_session)):
+async def get_all_requests(session: AsyncSession = Depends(get_async_session)):
     query = select(request).where(request.c.status == "working")
     result = await session.execute(query)
-    row = result.mappings().all()
-    return row
+    return result.mappings().all()
 
 
-@router.post("/")
-async def add_request(new_order: RequestCreate, session: AsyncSession = Depends(get_async_session)):
+@router.get("/{request_id}/")
+async def get_request_by_id(request_id: int, session: AsyncSession = Depends(get_async_session)):
+    print(request_id)
+    query = select(request).where(request.c.id == request_id)
+    result = await session.execute(query)
+    return result.mappings().all()
+
+
+@router.post("/add/")
+async def add_request(
+        new_order: RequestCreate,
+        session: AsyncSession = Depends(get_async_session),
+        user: User = Depends(current_user)
+):
     stmt = insert(request).values(**new_order.dict())
     await session.execute(stmt)
     await session.commit()
